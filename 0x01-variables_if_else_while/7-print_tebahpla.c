9c81323c48370ec284e8deac6a5a8e68718e1ff8
TshehlaThabo <tshehla68@gmail.com>
2022-12-09 18:23:45 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - lowercase alphabets reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for ( a = 'z'; a >= 'z'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

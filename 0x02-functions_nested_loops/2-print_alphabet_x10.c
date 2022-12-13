0b2cd904e6db8e91f7896544b6621b4f74a256d8
TshehlaThabo <tshehla68@gmail.com>
2022-12-13 10:05:01 -0800

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10x
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (( c <= 'z') && (i <= 9))
+	{
+		_putchar(c);
+		c++;
+		i++;
+	}
+	
+	_putcha('\n');
+}

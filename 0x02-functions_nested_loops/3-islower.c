cc505516838e1f75e1001c4faf5ed5d883ffe992
TshehlaThabo <tshehla68@gmail.com>
2022-12-13 10:31:24 -0800

    Task2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c: The character to be checked
  *
  * Return: 1 for lowercase character 0 for anything else
  */
 int _islower(int c)
 {
-    	if (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}

0f1dba1411ce255b6edcf6e9d93c5451bd019828
TshehlaThabo <tshehla68@gmail.com>
2022-12-16 09:13:10 -0800

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-#include <stdio>
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,5 +12,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif /*MAIN_H */

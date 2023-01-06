c5cc3fdf219a0fd8803196fe6440b75b8436a8f6
TshehlaThabo <tshehla68@gmail.com>
2023-01-06 15:43:08 -0800

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int find_sqrt(int num, int root); 
+int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
 
 /**
@@ -15,7 +15,7 @@ int _sqrt_recursion(int n);
 
 int find_sqrt(int num, int root)
 {
-	if ((root * root) == num) 
+	if ((root * root) == num)
 		return (root);
 
 	if (root == num / 2)

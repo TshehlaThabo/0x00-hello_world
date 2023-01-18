6c65afe995a1ac99b8dde2046481f219abe74635
TshehlaThabo <tshehla68@gmail.com>
2023-01-18 06:18:58 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include "function_pointers.h"
+
 
 /**
  * print_name - function to print name
@@ -12,7 +11,6 @@
 
 vioid print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
